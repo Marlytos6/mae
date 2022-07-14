f2070aee5612b7b99be5498bb41e754f759b202d
Marlytos6 <matheusmhirata5@gmail.com>
2022-07-14 03:45:52 -0300

    terminar as páginas de autenticação

--- Django/autenticacao/urls.py
+++ Django/autenticacao/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
+    path('cadastro/', views.cadastrar, name='cadastro'),
 ]
--- Django/autenticacao/views.py
+++ Django/autenticacao/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request, 'login.html')
+
+def cadastrar(request):
+    return render(request, 'registrar.html')
